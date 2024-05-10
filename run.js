b87d7583e09a3aad895694f96c76ecafdb2a8f73
tiahzap <tiahzap@gmail.com>
2024-05-11 06:55:54 +0700

    Add files via upload

---
+++ run.js
@@ -0,0 +1,440 @@
+const bluebird = require("bluebird");
+const fs = require("fs");
+const AWS = require("aws-sdk");
+require("aws-sdk/lib/maintenance_mode_message").suppress = true;
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+
+const IDRIVE_ENPOINT = "d4b6.or4.idrivee2-60.com";
+const IDRIVE_ACCESS_KEY_ID = "5ce3lLJdmdBVLqcX7RPD";
+const IDRIVE_SECRET_ACCESS_KEY = "a6USwSGtrxBDlV2n19XUJWcMt69ivtpu5zm63Hfi";
+const BUCKET_NAME = "alchemy-cookies";
+
+const CONCURENCY = 5;
+const HEADLESS = "new"; // HEADLESS  false or "new";
+
+const dataAccount = `
+
+santfe2003@fadsfg1d.shop,@@Masuk123#oZ,0xbfdfd097a515ea6e5da8f1837ac23f0079387a85
+dwgitt@fadsfg1d.shop,@@Masuk123#oZ,0x89056f02988b562944958053d0b85d899d9d3673
+herbertroot@fadsfg1d.shop,@@Masuk123#oZ,0x01694ed4cff65d5e7719c6a0db9ae503ffaf8940
+sp00nm4n@fadsfg1d.shop,@@Masuk123#oZ,0x6eb44416073bd567696d8ede53c1745a86b886c9
+tallegretti@fadsfg1d.shop,@@Masuk123#oZ,0x577b9a728f5097ccc889f956dccffce6e28a74f3
+josemerle@fadsfg1d.shop,@@Masuk123#oZ,0x129ffac8b90d69c4c7d517d7a03561a2bc9de263
+jesusjavier@fadsfg1d.shop,@@Masuk123#oZ,0xa3eea071c76f777d91f84edc3d65b693e0bbfd40
+vozoxetzex1964@fadsfg1d.shop,@@Masuk123#oZ,0x7f90f7c3f7c21e406633961e377f00a231fe9a87
+bolatabaev@fadsfg1d.shop,@@Masuk123#oZ,0xc545f3b9ce9e2a02d21238c379d305c0c7562fae
+536563@fadsfg1d.shop,@@Masuk123#oZ,0xec0e8bb1172a7b5e599955e94728fdf0d02cdd5d
+
+`;
+
+const waiting = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time));
+};
+
+const getData = (data, start, end) => {
+  const splitData = data.split(/\r?\n/).filter((n) => n);
+  const sliceData = splitData.slice(start, end);
+  return sliceData;
+};
+
+const s3 = () => {
+  const endpoint = new AWS.Endpoint(IDRIVE_ENPOINT);
+  const s3 = new AWS.S3({
+    endpoint: endpoint,
+    accessKeyId: IDRIVE_ACCESS_KEY_ID,
+    secretAccessKey: IDRIVE_SECRET_ACCESS_KEY,
+  });
+
+  return s3;
+};
+
+const existsBucket = async (bucketName) => {
+  try {
+    await listObjects(bucketName);
+
+    return true;
+  } catch (err) {
+    if (err.code == "NoSuchBucket") {
+      return false;
+    } else {
+      throw err;
+    }
+  }
+};
+
+const listObjects = (bucketName) => {
+  return new Promise((resolve, reject) => {
+    const data = {
+      Bucket: bucketName,
+    };
+
+    s3().listObjects(data, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const getObject = (bucketName, fileName) => {
+  return new Promise((resolve, reject) => {
+    const data = {
+      Bucket: bucketName,
+      Key: fileName,
+    };
+
+    s3().getObject(data, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const createObject = async (obj, bucketName, fileName) => {
+  const buf = Buffer.from(JSON.stringify(obj));
+
+  return new Promise((resolve, reject) => {
+    const data = {
+      Bucket: bucketName,
+      Key: fileName,
+      Body: buf,
+      ContentEncoding: "base64",
+      ContentType: "application/json",
+      ACL: "private",
+    };
+
+    s3().upload(data, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const checkExistsObject = async (bucketName, fileName) => {
+  try {
+    await getObject(bucketName, fileName);
+
+    return true;
+  } catch (err) {
+    if (err && (err.code == "NoSuchKey" || err.code == "NoSuchBucket"))
+      return false;
+  }
+};
+
+const saveCookies = async (page, cookieFile) => {
+  const session = await page.target().createCDPSession();
+  const reaponseCookies = await session.send("Network.getAllCookies");
+
+  await session.detach();
+  await createObject(reaponseCookies.cookies, BUCKET_NAME, cookieFile);
+};
+
+const loadCookies = async (page, cookieFile) => {
+  const session = await page.target().createCDPSession();
+  const cookies = JSON.parse(cookieFile);
+  await session.send("Network.setCookies", {
+    cookies: cookies,
+  });
+  await session.detach();
+};
+
+const retryElement = async (page, element, xpath = false, retryCount = 1) => {
+  try {
+    if (xpath) {
+      return await page.waitForXPath(element, { timeout: 8000 });
+    } else {
+      return await page.waitForSelector(element, { timeout: 8000 });
+    }
+  } catch (err) {
+    if (retryCount <= 0) {
+      throw err;
+    }
+    const currentUrl = await page.url();
+    await page.goto(currentUrl, { waitUntil: "networkidle2" });
+
+    return await retryElement(page, element, (xpath = false), retryCount - 1);
+  }
+};
+
+const getBrowser = async () => {
+  try {
+    let browser;
+
+    let args = [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--disable-notifications",
+      "--no-first-run",
+      "--disable-gpu",
+      // "--start-maximized",
+      "--disable-infobars",
+      "--disable-web-security",
+      "--ignore-certificate-errors",
+      "--disable-background-timer-throttling",
+      "--disable-backgrounding-occluded-windows",
+      "--disable-features=IsolateOrigins,site-per-process,SitePerProcess",
+      "--flag-switches-begin --disable-site-isolation-trials --flag-switches-end",
+    ];
+
+    let browserOptions = {
+      headless: HEADLESS,
+      ignoreHTTPSErrors: true,
+      acceptInsecureCerts: true,
+      defaultViewport: null,
+      args: args,
+    };
+
+    browser = await puppeteer.launch(browserOptions);
+    const context = browser.defaultBrowserContext();
+
+    context.overridePermissions("https://auth.alchemy.com", [
+      "geolocation",
+      "notifications",
+    ]);
+    context.overridePermissions("https://www.alchemy.com", [
+      "geolocation",
+      "notifications",
+    ]);
+
+    const [page] = await browser.pages();
+
+    await page.setRequestInterception(true);
+    const rejectRequestPattern = [
+      "googlesyndication.com",
+      "/*.doubleclick.net",
+      "/*.amazon-adsystem.com",
+      "/*.adnxs.com",
+      "/*.ads.net",
+    ];
+    const blockList = [];
+    page.on("request", (request) => {
+      if (
+        rejectRequestPattern.find((pattern) => request.url().match(pattern))
+      ) {
+        blockList.push(request.url());
+        request.abort();
+      } else request.continue();
+    });
+
+    return { page, browser };
+  } catch (err) {
+    console.log(`Browser ${err}`);
+  }
+};
+
+const login = async (page, email, password) => {
+  try {
+    await page.goto("https://www.alchemy.com/faucets/arbitrum-sepolia", {
+      waitUntil: "networkidle2",
+    });
+
+    const cookieFile = `${email}.json`;
+
+    const isExistCookies = await checkExistsObject(BUCKET_NAME, cookieFile);
+
+    if (isExistCookies) {
+      const getCookies = await getObject(BUCKET_NAME, cookieFile);
+      const cookies = getCookies.Body.toString("utf-8");
+      await loadCookies(page, cookies);
+    }
+
+    await waiting(5000);
+
+    const logoutButtonElm = await page.$$eval("button", (button) => {
+      const logoutButton = button.find(
+        (btn) => btn.textContent.trim() == "Logout"
+      );
+
+      if (logoutButton) {
+        return true;
+      }
+
+      return false;
+    });
+
+    if (logoutButtonElm) {
+      return true;
+    }
+
+    await page.$$eval("button", (button) => {
+      const loginButton = button.find(
+        (btn) => btn.textContent.trim() == "Alchemy Login"
+      );
+
+      if (loginButton) {
+        loginButton.focus();
+        loginButton.click();
+      }
+    });
+
+    await waiting(10000);
+
+    try {
+      await retryElement(page, 'input[type="email"]');
+
+      const inputUser = await page.$('input[type="email"]');
+      await page.evaluate((user) => {
+        user.focus();
+        user.click();
+      }, inputUser);
+      await page.keyboard.type(email);
+
+      const inputPass = await page.$('input[type="password"]');
+      await page.evaluate((pass) => {
+        pass.focus();
+        pass.click();
+      }, inputPass);
+      await page.keyboard.type(password);
+
+      await page.waitForSelector('button[type="submit"]');
+      const buttonLogin = await page.$('button[type="submit"]');
+
+      await page.evaluate((login) => {
+        login.click();
+      }, buttonLogin);
+
+      await waiting(15000);
+
+      await saveCookies(page, cookieFile);
+    } catch (err) {}
+
+    return true;
+  } catch (err) {
+    console.log(`[${email}] - Login error ${err}`);
+  }
+};
+const claimFoucet = async (page, email, wallet) => {
+  let success = false;
+  let retry = 0;
+  let maxTry = 3;
+  let message = "";
+
+  try {
+    while (!success && retry < maxTry) {
+      await waiting(2000);
+
+      await page.waitForSelector('form input[type="text"]');
+      const walletInputElm = await page.$('form input[type="text"]');
+
+      await page.evaluate((walletInput) => {
+        walletInput.focus();
+        walletInput.click();
+      }, walletInputElm);
+
+      await page.keyboard.down("Control");
+      await page.keyboard.press("A");
+      await page.keyboard.up("Control");
+      await page.keyboard.press("Backspace");
+      await page.keyboard.sendCharacter(wallet);
+
+      await page.waitForXPath('//div/button[contains(., "Send Me ETH")]');
+
+      const [sendButtonElm] = await page.$x(
+        '//div/button[contains(., "Send Me ETH")]'
+      );
+
+      await sendButtonElm.click();
+
+      await waiting(4000);
+
+      const successClaimElm = await page.$x(
+        '//*[@id="root"]/div[1]/div[2]/div[3]/div[2]/div/div[2]/div/div[2]'
+      );
+
+      if (successClaimElm !== "undefined" && successClaimElm.length > 0) {
+        console.log(`[${email}] - BERHASIL CLAIM ARBIT !!`);
+        success = true;
+        return true;
+      } else {
+        const [spanMessageElm] = await page.$x('//div[@role="alert"]/span');
+
+        let textMessage = await page.evaluate(
+          (element) => element.textContent.trim(),
+          spanMessageElm
+        );
+
+        message = textMessage;
+
+        retry++;
+
+        await waiting(3000);
+
+        const currentUrl = await page.url();
+        await page.goto(currentUrl, { waitUntil: "networkidle2" });
+      }
+    }
+
+    console.log(`[${email}] - GAGAL CLAIM ARBIT ${message}`);
+    return true;
+  } catch (err) {
+    console.log(`[${email}] - TERJADI ERROR: ${err}`);
+  }
+};
+
+const bot = async (page, account) => {
+  let success = false;
+  try {
+    await page.bringToFront();
+    const client = await page.target().createCDPSession();
+    await client.send("Network.clearBrowserCookies");
+    await client.send("Network.clearBrowserCache");
+    await client.send("Page.enable");
+    await client.send("Page.setWebLifecycleState", { state: "active" });
+
+    const data = account.split(",");
+    const email = data[0];
+    const password = data[1];
+    const wallet = data[2];
+
+    const sigin = await login(page, email, password);
+
+    if (sigin) {
+      success = await claimFoucet(page, email, wallet);
+    }
+
+    return success;
+  } catch (err) {
+    console.log(err);
+  }
+};
+(async () => {
+  const arguments = process.argv;
+
+  const startData = parseInt(arguments[2]);
+  const endData = parseInt(arguments[3]);
+
+  if (!startData && !endData) {
+    console.log(`Arguments require "node run.js 0 5"`);
+    process.exit();
+  }
+
+  const accounts = getData(dataAccount, startData, endData);
+
+  return bluebird.map(
+    accounts,
+    async (account) => {
+      const { page, browser } = await getBrowser();
+
+      try {
+        await bot(page, account);
+      } catch (err) {
+        await browser.close();
+      }
+
+      await browser.close();
+    },
+    { concurrency: CONCURENCY }
+  );
+})();
